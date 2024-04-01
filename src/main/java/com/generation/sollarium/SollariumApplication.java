ae543e7e6bd4891b2802ef9bc17de7e7dac644c1
tpaiva700 <thaisferreirapaiva19@gmail.com>
2024-04-01 10:18:59 -0300

    Iniciando o projeto Sollarium no Spring Boot

---
+++ src/main/java/com/generation/sollarium/SollariumApplication.java
@@ -0,0 +1,13 @@
+package com.generation.sollarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SollariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SollariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/sollarium/SollariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.sollarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SollariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
