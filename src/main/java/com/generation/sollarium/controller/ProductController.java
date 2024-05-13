642b83ba9308e8758f1d29efb08f35dcceaca03f
PedroHenrique726 <100783522+PedroHenrique726@users.noreply.github.com>
2024-05-13 11:32:35 -0300

    Arrumando B.o do Back 2.0

--- src/main/java/com/generation/sollarium/controller/ProductController.java
+++ src/main/java/com/generation/sollarium/controller/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.generation.sollarium.model.Product;
 import com.generation.sollarium.repository.CategoryRepository;
 import com.generation.sollarium.repository.ProductRepository;
